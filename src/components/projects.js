c8f3b9219091b85ef284c694bed014c144cf60ec
Juan David Orozco <ingjuanorozco@outlook.com>
2019-08-04 17:55:05 -0500

    Se añadio la página de proyectos

--- src/components/projects.js
+++ src/components/projects.js
@@ -1,12 +1,115 @@
 import React, { Component } from 'react';
+import { Tabs, Tab, Grid, Cell, Card, CardTitle, CardActions, Button, CardMenu, IconButton, CardText} from 'react-mdl';
 
 class Projects  extends Component{
+    constructor (props){
+        super(props);
+        this.state = { activeTab:0};
+    }
+    toggleCategories(){
+        if(this.state.activeTab === 0){
+            return(
+                <div className ="projects-grid">
+                    <Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
+                <CardTitle style={{color: '#fff', height: '196px', background: 'url(https://cdn.worldvectorlogo.com/logos/react-1.svg) center / cover'}}>React Project #1</CardTitle>
+                <CardText>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</CardText>
+                <CardActions border>
+                    <Button colored>Github</Button>
+                    <Button colored>CodePen</Button>
+                </CardActions>
+                <CardMenu style ={{color: '#fff'}}>
+                    <IconButton name="share"></IconButton>
+                </CardMenu>
+            </Card>
+            <Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
+                <CardTitle style={{color: '#fff', height: '196px', background: 'url(https://cdn.worldvectorlogo.com/logos/react-1.svg) center / cover'}}>React Project #2</CardTitle>
+                <CardText>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</CardText>
+                <CardActions border>
+                    <Button colored>Github</Button>
+                    <Button colored>CodePen</Button>       
+                </CardActions>
+                <CardMenu style ={{color: '#fff'}}>
+                    <IconButton name="share"></IconButton>
+                </CardMenu>
+            </Card>
+            <Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
+                <CardTitle style={{color: '#fff', height: '196px', background: 'url(https://cdn.worldvectorlogo.com/logos/react-1.svg) center / cover'}}>React Project #3</CardTitle>
+                <CardText>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</CardText>
+                <CardActions border>
+                    <Button colored>Github</Button>
+                    <Button colored>CodePen</Button>
+                </CardActions>
+                <CardMenu style ={{color: '#fff'}}>
+                    <IconButton name="share"></IconButton>
+                </CardMenu>
+            </Card>
+                </div>
+               
+            )
+        }
+        else if (this.state.activeTab ===1){
+            return(
+                <Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
+                <CardTitle style={{color: '#fff', height: '196px', background: 'url(https://cdn.worldvectorlogo.com/logos/react-1.svg) center / cover'}}>React Project #1</CardTitle>
+                <CardText>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</CardText>
+                <CardActions border>
+                    <Button colored>Github</Button>
+                    <Button colored>CodePen</Button>
+                </CardActions>
+                <CardMenu style ={{color: '#fff'}}>
+                    <IconButton name="share"></IconButton>
+                </CardMenu>
+            </Card>
+            )
+        }
+        else if (this.state.activeTab ===2){
+            return(
+                <Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
+                <CardTitle style={{color: '#fff', height: '196px', background: 'url(https://cdn.worldvectorlogo.com/logos/react-1.svg) center / cover'}}>React Project #1</CardTitle>
+                <CardText>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</CardText>
+                <CardActions border>
+                    <Button colored>Github</Button>
+                    <Button colored>CodePen</Button>
+                </CardActions>
+                <CardMenu style ={{color: '#fff'}}>
+                    <IconButton name="share"></IconButton>
+                </CardMenu>
+            </Card>
+            )
+        }
+        else if (this.state.activeTab ===3){
+            return(
+                <Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
+                <CardTitle style={{color: '#fff', height: '196px', background: 'url(https://cdn.worldvectorlogo.com/logos/react-1.svg) center / cover'}}>React Project #1</CardTitle>
+                <CardText>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</CardText>
+                <CardActions border>
+                    <Button colored>Github</Button>
+                    <Button colored>CodePen</Button>
+                </CardActions>
+                <CardMenu style ={{color: '#fff'}}>
+                    <IconButton name="share"></IconButton>
+                </CardMenu>
+            </Card>
+            )
+        }
+    }
+    
 render () {
     return (
-        <div>
-            <h1>
-                Projects page
-            </h1>
+        <div className ="category-tabs">
+            <Tabs activeTb = {this.state.activeTab} onChange ={(tabId) => this.setState({activeTab: tabId})} ripple>
+                <Tab>React</Tab>
+                <Tab>Angular</Tab>
+                <Tab>VueJS</Tab>
+                <Tab>MongoDB</Tab>
+            </Tabs>
+                
+                <Grid >
+                <Cell col ={12}>
+                    <div className="content">{this.toggleCategories()}</div>
+                </Cell>
+                </Grid>
+            
         </div>
     )
 }
