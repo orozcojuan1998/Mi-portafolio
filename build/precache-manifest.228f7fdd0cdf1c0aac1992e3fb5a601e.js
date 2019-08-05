170f0cbbc805a2100961981948ae395846c51237
Juan David Orozco <ingjuanorozco@outlook.com>
2019-08-04 20:17:02 -0500

    Build files

---
+++ build/precache-manifest.228f7fdd0cdf1c0aac1992e3fb5a601e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94e71654ba90a8edc0bbf1f1b7950e3a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e1d72cb58433cb6a91bc",
+    "url": "/static/css/2.a64ea057.chunk.css"
+  },
+  {
+    "revision": "a559a76cb8f34c72d974",
+    "url": "/static/css/main.4205b7e7.chunk.css"
+  },
+  {
+    "revision": "e1d72cb58433cb6a91bc",
+    "url": "/static/js/2.dee77d30.chunk.js"
+  },
+  {
+    "revision": "a559a76cb8f34c72d974",
+    "url": "/static/js/main.32c1a481.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
