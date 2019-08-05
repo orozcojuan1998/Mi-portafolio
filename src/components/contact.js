24e713d3de524efafd12a3a9fddb47262f2dda27
Juan David Orozco <ingjuanorozco@outlook.com>
2019-08-04 19:57:35 -0500

    Se terminó pagina de contacto y se empezó página de hoja de vida.

--- src/components/contact.js
+++ src/components/contact.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Grid, Cell} from 'react-mdl'
+import { Grid, Cell, List, ListItem, ListItemContent} from 'react-mdl'
 
 class Contact  extends Component{
 render () {
@@ -7,10 +7,40 @@ render () {
         <div className="contact-body">
         <Grid className="contact-grid">
         <Cell col={6}>
-            half page
+            <h2>Juan David Orozco</h2>
+            <img 
+                src ="https://cdn2.iconfinder.com/data/icons/avatar-2/512/Fred_man-512.png"
+                alt ="avatar"
+                style = {{height: '250px'}}
+            />
+            <p style ={{width: '75%', margin: 'auto', paddingTop: '1em'}}>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
         </Cell>
         <Cell col={6}>
-            half page
+            <h2>Contáctame</h2>
+            <hr/>
+            <div className = "contact-list">
+                <List>
+                <ListItem>
+                    <ListItemContent style = {{fontSize: '28px', fontFamily: 'Anton'}}>
+                    <i className="fa fa-phone-square" aria-hidden="true"></i>
+                         +57 3225686346
+                    </ListItemContent>
+                </ListItem>
+                <ListItem>
+                    <ListItemContent style = {{fontSize: '28px', fontFamily: 'Anton'}}>
+                    <i className="fa fa-envelope" aria-hidden="true"></i>
+                         orozco_juan@javeriana.edu.co
+                    </ListItemContent>
+                </ListItem>
+                <ListItem>
+                    <ListItemContent style = {{fontSize: '28px', fontFamily: 'Anton'}}>
+                    <i className="fa fa-facebook-square" aria-hidden="true"></i>
+                         www.fb.com/ingjuanorozco
+                    </ListItemContent>
+                </ListItem>
+                </List>
+            </div>
+            
         </Cell>
         </Grid>
            
--- src/components/resume.js
+++ src/components/resume.js
@@ -1,12 +1,41 @@
 import React, { Component } from 'react';
+import { Grid, Cell } from 'react-mdl';
+import Education from './education';
 
 class Resume  extends Component{
 render () {
     return (
         <div>
-            <h1>
-                Resume page
-            </h1>
+           <Grid>
+               <Cell col={4}>
+                    <div style ={{textAlign: 'center'}}>
+                    <img 
+                    src ="https://www.shareicon.net/download/2015/09/18/103157_man_512x512.png"
+                    alt ="avatar"
+                    style = {{height: '200px'}}
+                    />
+                    <h2 style={{paddingTop:'2em'}}>Juan David Orozco</h2>
+                    <h4 style = {{color : 'grey'}}>Ingeniero de sistemas</h4>
+                    <hr style = {{borderTop: '3px solid #833fb2', width: '60%'}}/>
+                    <p>Novio de Angie</p>
+                    <hr style = {{borderTop: '3px solid #833fb2', width: '60%'}}/>
+                    <h5>Dirección</h5>
+                    <p>Carrera 98A#68A-15</p>
+                    <h5>Teléfono</h5>
+                    <p>3225686346</p>
+                    <h5>Correo</h5>
+                    <p>orozco_juan@javeriana.edu.co</p>
+                    <h5>Sitio Web</h5>
+                    <p>www.mysite.com</p>
+                    <hr style = {{borderTop: '3px solid #833fb2', width: '60%'}}/>
+
+                    </div>
+               </Cell>
+               <Cell className="resume-right-col" col={8}>
+                    <h2>Educación</h2>
+                    <Education/>
+               </Cell>
+           </Grid>
         </div>
     )
 }
